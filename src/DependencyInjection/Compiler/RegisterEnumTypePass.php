14fa39c089d92c5bf55b797b6fae1f488e52106b
Daniel Bojdo <daniel.bojdo@dxi.eu>
2015-06-04 14:59:16 +0100

    [new] register enum type pass

---
+++ src/DependencyInjection/Compiler/RegisterEnumTypePass.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * RegisterEnumTypePass.php
+ *
+ * @author dbojdo - Daniel Bojdo <daniel.bojdo@dxi.eu>
+ * Created on Jun 04, 2015, 14:56
+ * Copyright (C) DXI Ltd
+ */
+
+namespace Dxi\DoctrineExtensionBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * Class RegisterEnumTypePass
+ * @package Dxi\DoctrineExtensionBundle\DependencyInjection\Compiler
+ */
+class RegisterEnumTypePass implements CompilerPassInterface
+{
+    /**
+     * @var array
+     */
+    private $enumMap;
+
+    /**
+     * @param array $enumMap
+     */
+    public function __construct(array $enumMap)
+    {
+        $this->enumMap = $enumMap;
+    }
+
+    /**
+     * You can modify the container here before it is dumped to PHP code.
+     *
+     * @param ContainerBuilder $container
+     *
+     * @api
+     */
+    public function process(ContainerBuilder $container)
+    {
+        $types = $container->getParameter('dxi_doctrine_extension.enum.types');
+        $types = array_replace($types, $this->enumMap);
+
+        $container->setParameter('dxi_doctrine_extension.enum.types', $types);
+    }
+}
